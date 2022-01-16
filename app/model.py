3866c1ceded423e23002ae09a76c129b59f459f9
Tyler Nie <tylernie@live.ca>
2022-01-16 12:12:08 -0800

    added quantity to Item model

--- app/model.py
+++ app/model.py
@@ -6,6 +6,7 @@ class Item(db.model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(60), unique=True)
+    quantity = db.Column (db.Integer)
     description = db.Column(db.String(300))
 
     def __repr__(self):
