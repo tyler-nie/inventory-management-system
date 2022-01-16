a6465a82b5891bd7141b19777b0431e41eaabad6
Tyler Nie <tylernie@live.ca>
2022-01-15 22:03:25 -0800

    finish initial app

--- app/__init__.py
+++ app/__init__.py
@@ -13,5 +13,9 @@ def create_app(config_name):
     app.config.from_pyfile('config.py')
     db.init_app(app)
 
+    @app.route('/')
+    def hello_world():
+        return 'Hello, World!'
+
     return app
 
