5f8c8bc6d2c448c19c70c90c52b18c4554277094
Tyler Nie <tylernie@live.ca>
2022-01-16 23:21:39 -0800

    implement export to csv

--- app/items/views.py
+++ app/items/views.py
@@ -1,11 +1,15 @@
 # Imports
+import csv
 from flask import abort, flash, redirect, render_template, url_for
 from .forms import ItemForm
 from .. import db
 from ..model import Item
 from . import items_blueprint
+from io import StringIO
 
-@items_blueprint.route('/', methods = ['GET', 'POST'])
+from werkzeug.wrappers import Response
+
+@items_blueprint.route('/', methods=['GET', 'POST'])
 def list_items():
     """
     List all items
@@ -14,7 +18,7 @@ def list_items():
     items = Item.query.all()
 
     return render_template('items/items.html',
-                          items = items, title="Items")
+                          items=items, title="Items")
 
 @items_blueprint.route('/add', methods=['GET', 'POST'])
 def add_item():
@@ -26,9 +30,9 @@ def add_item():
 
     form = ItemForm()
     if form.validate_on_submit():
-        item = Item(name = form.name.data,
-                    quantity = form.quantity.data,
-                    description = form.description.data)
+        item = Item(name=form.name.data,
+                    quantity=form.quantity.data,
+                    description=form.description.data)
         
         try:
             # add item to the database
@@ -43,7 +47,7 @@ def add_item():
         return redirect(url_for('items.list_items'))
 
     # load item template
-    return render_template('items/item.html', action = "Add", add_item = add_item, form = form, title = "Add Item")
+    return render_template('items/item.html', action="Add", add_item=add_item, form=form, title="Add Item")
 
 @items_blueprint.route('/edit/<int:id>', methods=['GET', 'POST'])
 def edit_item(id):
@@ -69,7 +73,7 @@ def edit_item(id):
     form.quantity.data = item.quantity
     form.name.data = item.name
     # load item template
-    return render_template('items/item.html', action ="Edit", add_item = add_item, form = form, item = item, title = "Edit Item")
+    return render_template('items/item.html', action ="Edit", add_item=add_item, form=form, item=item, title="Edit Item")
 
 @items_blueprint.route('/delete/<int:id>', methods=['GET', 'POST'])
 def delete_item(id):
@@ -86,3 +90,38 @@ def delete_item(id):
     return redirect(url_for('items.list_items'))
 
     # return render_template(title = "Delete Item")
+
+def generate_csv(items):
+        """
+        Generate a csv file
+        """
+
+        data = StringIO()
+        write = csv.writer(data)
+        
+        # write the header
+        write.writerow(['Name', 'Quantity', 'Description'])
+        yield data.getvalue()
+        data.seek(0)
+        data.truncate(0)
+
+        # write each item in items
+        for item in items:
+            write.writerow([item.name, item.quantity, item.description])
+            yield data.getvalue()
+            data.seek(0)
+            data.truncate(0)
+
+@items_blueprint.route('/export', methods=['GET', 'POST'])
+def export():
+    """
+    Export Items as csv
+    """
+    items = Item.query.all()
+
+    response = Response(generate_csv(items), mimetype='text/csv')
+
+    # add a filename
+    response.headers.set("Content-Disposition", "attachment", filename="items.csv")
+    return response
+    
